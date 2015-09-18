d0271516eefef296c855a6aeb121fc657a4753ca
liukun1016 <liukun1016@gmail.com>
2015-09-18 14:50:02 -0400

    Playlist

---
+++ YouTube/utility/environment.py
@@ -0,0 +1,15 @@
+# Constant for all parameters
+####MySQL database and tables
+DB_HOST = "localhost"
+DB_USER = 'root'
+DB_PASSWD = ''
+DB_NAME = "insight"
+DB_TB_CHANNEL_CATEGORY = "tb_channel_category"
+DB_TB_CHANNEL = 'tb_channel'
+DB_TB_PLAYLIST = 'tb_playlist'
+DB_TB_VIDEO='tb_video'
+
+# API parameter
+API_HOME = "https://www.googleapis.com/youtube/v3/"
+API_KEY = "AIzaSyAD4GzwWB16ilVKfsDymCfaZBodtUzA7-Y"
+MAX_RESULT = 50
